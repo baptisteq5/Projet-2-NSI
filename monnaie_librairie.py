2f54a1bd996368d775ba03cc22c7b1863b41625e
baptisteq5 <150150544+baptisteq5@users.noreply.github.com>
2023-12-12 14:00:05 +0100

    Update monnaie_librairie.py

--- monnaie_librairie.py
+++ monnaie_librairie.py
@@ -1,12 +1,11 @@
 def monnaie_librairie(monnaie_a_rendre):
-    rendre = monnaie_a_rendre
     liste_monnaie = [1, 2, 5, 10, 20, 50, 100, 200, 500]
     billets = []
     euro = liste_monnaie[len(liste_monnaie) - 1]
-    while rendre > 0:
-        if euro <= rendre:
+    while monnaie_a_rendre > 0:
+        if euro <= monnaie_a_rendre:
             billets.append(euro)
-            rendre -= euro
+            monnaie_a_rendre -= euro
         else:
             del liste_monnaie[len(liste_monnaie) - 1]
             euro = liste_monnaie[len(liste_monnaie) - 1]
