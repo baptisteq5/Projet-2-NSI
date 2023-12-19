f169be093138068f2af254ff5ef0e193c46ee7a5
baptisteq5 <150150544+baptisteq5@users.noreply.github.com>
2023-12-19 13:49:16 +0100

    Update baguette.py

--- baguette.py
+++ baguette.py
@@ -22,16 +22,16 @@ def baguette(gallions: int, mornilles: int, noises: int):
     version optimisée des sommes entrées en paramètres.
 
     '''
-    assert type(gallions) == int, "le paramètre gallions doit être un entier"
-    assert type(mornilles) == int, "le paramètre mornilles doit être un entier"
-    assert type(noises) == int, "le paramètre noises doit être un entier"
-    tot_noises = noises + 29 * mornilles + 493 * gallions
+    assert type(gallions) == int, "Le paramètre gallions doit être un entier"
+    assert type(mornilles) == int, "Le paramètre mornilles doit être un entier"
+    assert type(noises) == int, "Le paramètre noises doit être un entier"
+    total_noises = noises + 29 * mornilles + 493 * gallions
     a_rendre = {'gallions': 0, 'mornilles': 0, 'noises': 0}
-    while tot_noises >= 493:
+    while total_noises >= 493:
         a_rendre['gallions'] += 1
-        tot_noises -= 493
-    while tot_noises >= 29:
+        total_noises -= 493
+    while total_noises >= 29:
         a_rendre['mornilles'] += 1
-        tot_noises -= 29
-    a_rendre['noises'] += tot_noises
+        total_noises -= 29
+    a_rendre['noises'] += total_noises
     return a_rendre
